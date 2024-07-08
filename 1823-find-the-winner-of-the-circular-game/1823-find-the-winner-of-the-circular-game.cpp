38fdb362c832024b510019ef4c42a090b40d54ce
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-07-09 02:30:18 +0530

    Time: 2 ms (75.49%), Space: 7.1 MB (92.64%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -2,27 +2,11 @@ class Solution {
 public:
     
     
-    int findTheWinnerIdx(int n,int k){
-        
-        if(n==1){
-            return 0;
-        }
-        
-        int Idx=findTheWinnerIdx(n-1,k);
-        
-        Idx=(Idx+k)%n; // to find the original index int original array
-        
-        return Idx;
+   
+   int findTheWinner(int n, int k, int val = 0) {
+        for(int i =2; i<n+1;i++)
+            val = (val+k) % i;
+        return val+1;
     }
-    
-    int findTheWinner(int n, int k) {
-        
-        
-        int result_idx=findTheWinnerIdx(n,k);
-        
-        return result_idx +1;
-        
-        // T.C=O(n)
-        
-    }
+   
 };
