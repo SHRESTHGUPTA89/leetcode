5e7a467354cd3c3d77e40b343fa5e3c2c7aa38bd
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-07-09 01:55:44 +0530

    Time: 24 ms (22.83%), Space: 25.7 MB (9.27%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        // Create a list of friends from 1 to n
-        vector<int> arr;
-        for (int i = 0; i < n; i++) {
-            arr.push_back(i + 1);
-        }
-
-        // Initialize the current index to start counting from
-        int index = 0;
-
-        // Repeat the process until one person is left
-        while (arr.size() > 1) {
-            // Calculate the index of the friend to remove
-            index = (index + k - 1) % arr.size();
-            arr.erase(arr.begin() + index);
-        }
-
-        // Return the winner
-        return arr[0];
         
-         
-    }//  time comp= O(n*n)
+        
+        queue<int>que;
+        
+        for(int i=1;i<=n;i++){
+            que.push(i);
+        }
+        
+        while(que.size() > 1){
+            
+            for(int count=1;count<=k-1;count++){
+                que.push(que.front());
+                que.pop();
+            }
+            que.pop();
+        }
+        return que.front();
+        // T.C= O(n*k)
+    }
 };
