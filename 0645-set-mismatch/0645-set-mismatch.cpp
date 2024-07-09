a8bd6233957297626b3fc05fc54a2c4123884802
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-07-09 21:46:43 +0530

    Time: 22 ms (63.92%), Space: 23.8 MB (83.97%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        
+        
+        int n=nums.size();
+        int i=0;
+        vector<int> ans;
+        
+        while(i<n){
+            
+            int correct= nums[i]  - 1; // tell us correct index for nums in sorting terms
+            
+            if(  nums[i] != nums[correct]){
+                
+                swap(nums[i] , nums[correct] );
+                
+            }
+            else{
+                i++;
+            }   
+        }
+        
+        for(int i=0;i<n;i++){
+            
+            if(nums[i]!=i+1){
+                ans.push_back(nums[i]);
+                ans.push_back(i+1);
+            }
+        }
+        return ans;
+    }    
+};
+        
+ 
