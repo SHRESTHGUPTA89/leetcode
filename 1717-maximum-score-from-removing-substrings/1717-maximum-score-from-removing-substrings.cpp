4e2cc60fce9a8b8897e981ba6b037b4ebd495756
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-07-12 22:45:26 +0530

    Time: 115 ms (24.80%), Space: 30.4 MB (10.16%) - LeetHub

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
@@ -1,43 +1,44 @@
 class Solution {
 public:
-    int maximumGain(string s, int x, int y) {
-        // Ensure "ab" always has more points than "ba"
-        if (x < y) {
-            // Swap points
-            int temp = x;
-            x = y;
-            y = temp;
-            // Reverse the string to maintain logic
-            reverse(s.begin(), s.end());
-        }
-
-        int aCount = 0, bCount = 0, totalPoints = 0;
-
-        for (int i = 0; i < s.size(); ++i) {
-            char currentChar = s[i];
-
-            if (currentChar == 'a') {
-                ++aCount;
-            } else if (currentChar == 'b') {
-                if (aCount > 0) {
-                    // Can form "ab", remove it and add points
-                    --aCount;
-                    totalPoints += x;
-                } else {
-                    // Can't form "ab", keep 'b' for potential future "ba"
-                    ++bCount;
-                }
+    string removeSubstr(string &s, string &matchStr){
+        stack<char> st;
+        
+        for (char &ch : s) {
+            if (ch == matchStr[1] && !st.empty() && st.top() == matchStr[0]) {
+                st.pop();
             } else {
-                // Non 'a' or 'b' character encountered
-                // Calculate points for any remaining "ba" pairs
-                totalPoints += min(bCount, aCount) * y;
-                // Reset counters for next segment
-                aCount = bCount = 0;
+                st.push(ch);
             }
         }
-        // Calculate points for any remaining "ba" pairs at the end
-        totalPoints += min(bCount, aCount) * y;
-
-        return totalPoints;
+        
+        string temp;
+        while (!st.empty()) {
+            temp.push_back(st.top());
+            st.pop();
+        }
+        
+        reverse(temp.begin(), temp.end());
+        return temp;
+    }
+    
+    int maximumGain(string s, int x, int y) {
+        int n = s.length();
+        int score = 0;
+        
+        string maxStr = (x > y) ? "ab" : "ba";
+        string minStr = (x > y) ? "ba" : "ab";
+        
+        string temp_first = removeSubstr(s, maxStr);
+        int L = temp_first.length();
+        
+        int charRemoved = n - L;
+        score += (charRemoved / 2) * max(x, y);
+        
+        string temp_second = removeSubstr(temp_first, minStr);
+        charRemoved = L - temp_second.length();
+        
+        score += (charRemoved / 2) * min(x, y);
+        
+        return score;
     }
 };
