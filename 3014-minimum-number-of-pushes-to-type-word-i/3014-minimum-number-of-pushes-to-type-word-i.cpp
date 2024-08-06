8c360e0f94039cc61cb04835c429fcbc27bb6c0f
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-08-06 17:12:08 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (9.12%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
-    int minimumPushes(string word) 
-    {
-        int n=word.size();
-        int cnt1=0,cnt2=0;
-        string s=word;
-        for(int i=0;i<n;++i)
-        {
-            if(s[i]=='1'||s[i]=='#'||s[i]=='0'||s[i]=='*') cnt1++;
-            else cnt2++;
-        }
-        if(cnt2<=8)
-        {
-            return cnt1+cnt2;
-        }
-        else if(cnt2>8 and cnt2<=16)
-        {
-            return cnt1+8+(cnt2-8)*2;
-        }
-        else if(cnt2>16 and cnt2<=24)
-        {
-            return cnt1+8+16+(cnt2-16)*3;
-        }
-        else if(cnt2>=24 and cnt2<=26)
-        {
-            return cnt1+8+16+24+(cnt2-24)*4;
-        }
-        return 0;
+    int minimumPushes(string word) {
         
+        int result =0;
+        
+        unordered_map<int,int> mp;
+        int assign_key =2;
+        
+        for(char&ch: word){
+            if(assign_key > 9){
+                assign_key=2;
+            }
+            mp[assign_key]++;
+            result += mp[assign_key];
+            assign_key++;
+        }
+        
+        return result;
     }
 };
