2976eaf6c5b51fb92cf808ceaf38a10dcfc52d5a
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-08-07 14:55:40 +0530

    Time: 4 ms (70.13%), Space: 12.1 MB (43.03%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,26 +1,22 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans;
-
-        for (int i = 0; i < nums1.size(); i++) {
-            int nextGreater = -1; // Default value if no greater element is found
-            bool found = false; // Flag to indicate if nums1[i] has been found in nums2
-
-            for (int j = 0; j < nums2.size(); j++) {
-                if (nums2[j] == nums1[i]) {
-                    found = true;
-                }
-                if (found && nums2[j] > nums1[i]) {
-                    nextGreater = nums2[j];
-                    break;
-                }
+        map<int, int> nextGreater;
+        stack<int> s;
+        for(auto&num : nums2){
+            while (!s.empty() && s.top() < num){
+                nextGreater[s.top()] = num;
+                s.pop();
             }
-
-            ans.push_back(nextGreater);
+            s.push(num);
+        }
+        vector<int> ans;
+        for(auto& num : nums1){
+            if(nextGreater.find(num) != nextGreater.end()){
+                ans.push_back(nextGreater[num]);
+            }else{
+                ans.push_back(-1);
+            }
         }
 
         return ans;
