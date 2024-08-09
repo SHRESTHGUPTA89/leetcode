5512164e5da05e27d456be8e0a1887eed643892e
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-08-10 01:20:33 +0530

    Time: 27 ms (29.35%), Space: 26.5 MB (78.72%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
-    vector<int> nextGreaterElements(vector<int>& a) {
-        int n = a.size();
-        vector<int>v(n,-1);
-
-        stack<int>st;
-        for(int i = 2*n - 1; i >= 0; i--)
-        {
-            // we pop out all elements smaller than current element
-            while(!st.empty() && (a[i%n] >= st.top()))
-            {
-                st.pop();
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, -1);  // Initialize the answer vector with -1s
+        stack<int> s;  // Stack to store indices
+        
+        for (int i = 0; i < 2 * n; i++) {  // Loop through the array twice
+            int num = nums[i % n];  // Get the element in circular fashion
+            while (!s.empty() && nums[s.top()] < num) {
+                ans[s.top()] = num;  // Set the next greater element
+                s.pop();  // Remove the index from the stack
             }
-
-            // if stack is empty means no greater element is there
-            // if not empty we make answer at that index equal to top element
-            if(!st.empty() && (i < n))
-            {
-                v[i] = st.top();
+            if (i < n) {
+                s.push(i);  // Only push indices from the first pass
             }
-
-            st.push(a[i%n]);
         }
-
-        return v;
+        
+        return ans;
     }
 };
