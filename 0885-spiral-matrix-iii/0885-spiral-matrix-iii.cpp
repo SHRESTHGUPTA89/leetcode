1e59c20d27bef6e5653b2c465c90e65cc3a9258c
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-08-09 19:15:05 +0530

    Time: 10 ms (50.38%), Space: 13.9 MB (12.28%) - LeetHub

--- 0885-spiral-matrix-iii/0885-spiral-matrix-iii.cpp
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.cpp
@@ -1,31 +1,36 @@
+//Simple SImulation
+//T.C : O(max(rows,cols)^2)
+//S.C : O(1) (I am not considering the result matrix we have to return as result)
 class Solution {
 public:
-    vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart,
-                                        int cStart) {
-        // Store all possible directions in an array.
-        vector<vector<int>> dir{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        vector<vector<int>> traversed;
+    vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        vector<vector<int>> directions =  {
+                                            {0, 1},  //EAST
+                                            {1, 0},  //SOUTH
+                                            {0, -1}, //WEST
+                                            {-1, 0}  //NORTH
+                                          };
+        
+        vector<vector<int>> result;  
+        int step = 0; //how much steps to move
+        int dir  = 0; //Which direction
 
-        // Initial step size is 1, value of d represents the current direction.
-        for (int step = 1, direction = 0; traversed.size() < rows * cols;) {
-            // direction = 0 -> East, direction = 1 -> South
-            // direction = 2 -> West, direction = 3 -> North
-            for (int i = 0; i < 2; ++i) {
-                for (int j = 0; j < step; ++j) {
-                    // Validate the current position
-                    if (rStart >= 0 && rStart < rows && cStart >= 0 &&
-                        cStart < cols) {
-                        traversed.push_back({rStart, cStart});
-                    }
-                    // Make changes to the current position.
-                    rStart += dir[direction][0];
-                    cStart += dir[direction][1];
-                }
+        result.push_back({rStart, cStart});
 
-                direction = (direction + 1) % 4;
+        while (result.size() < rows * cols) {
+            //When we move EAST or WEST, we need to increase our steps by 1
+            if (dir == 0 || dir == 2) step++;
+
+            for (int i = 0; i < step; i++) {
+                rStart += directions[dir][0];
+                cStart += directions[dir][1];
+
+                if (rStart >= 0 && rStart < rows && cStart >= 0 && cStart < cols) // check valid
+                    result.push_back({rStart, cStart});
             }
-            ++step;
+
+            dir = (dir + 1) % 4; // turn to next direction
         }
-        return traversed;
+        return result;
     }
 };
