878699ef379cf2e277849f21fbe719378f77757b
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-08-23 21:32:03 +0530

    Time: 8 ms (92.81%), Space: 8.2 MB (90.62%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
-    std::string longestPalindrome(std::string s) {
-        if (s.length() <= 1) {
-            return s;
-        }
-
-        auto expand_from_center = [&](int left, int right) {
-            while (left >= 0 && right < s.length() && s[left] == s[right]) {
-                left--;
-                right++;
-            }
-            return s.substr(left + 1, right - left - 1);
-        };
-
-        std::string max_str = s.substr(0, 1);
-
-        for (int i = 0; i < s.length() - 1; i++) {
-            std::string odd = expand_from_center(i, i);
-            std::string even = expand_from_center(i, i + 1);
-
-            if (odd.length() > max_str.length()) {
-                max_str = odd;
-            }
-            if (even.length() > max_str.length()) {
-                max_str = even;
-            }
-        }
-
-        return max_str;
+    
+    // Helper function to expand around the center and find the length of the palindrome
+int expandAroundCenter(const string &s, int left, int right) {
+    while (left >= 0 && right < s.length() && s[left] == s[right]) {
+        left--;
+        right++;
     }
+    return right - left - 1; // Length of the palindrome
+}
+    string longestPalindrome(string str) {
+
+    if (str.empty()) return ""; // Handle empty string case
+    int start = 0, end = 0;
+
+    for (int i = 0; i < str.length(); i++) {
+        // Consider odd-length palindromes (centered at a single character)
+        int len1 = expandAroundCenter(str, i, i);
+        // Consider even-length palindromes (centered between two characters)
+        int len2 = expandAroundCenter(str, i, i + 1);
+        // Find the maximum length palindrome
+        int len = max(len1, len2);
+        
+        // Update start and end positions if a longer palindrome is found
+        if (len > end - start + 1) {
+            start = i - (len - 1) / 2;
+            end = i + len / 2;
+        }
+    }
+
+    // Extract the longest palindromic substring
+    return str.substr(start, end - start + 1);
+}
+    
 };
