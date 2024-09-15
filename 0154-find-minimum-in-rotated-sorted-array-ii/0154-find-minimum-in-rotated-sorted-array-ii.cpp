bb0976e535127a5192209bf04d38723671b64a70
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-09-15 22:43:31 +0530

    Time: 3 ms (81.24%), Space: 14.9 MB (90.93%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    int findMin(vector<int>& arr) {
-        int n = arr.size();
-        int s = 0;
-        int e = n - 1;
-
-        while (s < e) {
-            int m = s + (e - s) / 2;
-
-            // If arr[m] is greater than arr[e], the minimum is in the right half
-            if (arr[m] > arr[e]) {
-                s = m + 1;
+    int findMin(vector<int>& nums) {
+        
+        int n=nums.size();
+        
+       
+        int ans=nums[0];
+        for(int i=0;i<n;i++){
+            if(nums[i]<=ans){
+                ans=nums[i];
             }
-            // If arr[m] is less than arr[e], the minimum is in the left half (including mid)
-            else if (arr[m] < arr[e]) {
-                e = m;
-            }
-            // When arr[m] == arr[e], we can't determine which half to go,
-            // so we reduce the search space from the right by decrementing e.
-            else {
-                e--;
-            }
+            
         }
-
-        // After the loop, s == e, which is the index of the minimum element
-        return arr[s];
+        
+        return ans;
+        
+        
     }
 };
