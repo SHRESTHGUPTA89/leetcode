b4de9c3c62e99a367403096e07782704bb160ca4
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-09-29 23:31:32 +0530

    Time: 14 ms (55.11%), Space: 19.2 MB (16.91%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -1,30 +1,35 @@
+#include <queue>
+
 class Solution {
 public:
     Node* connect(Node* root) {
         if (root == nullptr) return root;  // Base case: empty tree
-        
-        Node* level_start = root;  // Start from the root (level 0)
-        
-        while (level_start->left != nullptr) {  // While there is a next level
-            Node* current = level_start;  // Iterate over the current level
+
+        std::queue<Node*> q;
+        q.push(root);  // Start with the root node
+
+        // BFS (level-order traversal)
+        while (!q.empty()) {
+            int size = q.size();  // Number of nodes at the current level
             
-            while (current != nullptr) {
-                // Connect the left child to the right child
-                current->left->next = current->right;
+            // Traverse the nodes at this level
+            for (int i = 0; i < size; i++) {
+                Node* current = q.front();  // Get the front node
+                q.pop();
                 
-                // Connect the right child to the next node's left child (if exists)
-                if (current->next != nullptr) {
-                    current->right->next = current->next->left;
+                // Set the `next` pointer of the current node
+                if (i < size - 1) {
+                    current->next = q.front();  // Connect to the next node in the queue
+                } else {
+                    current->next = nullptr;  // Last node of this level points to NULL
                 }
                 
-                // Move to the next node in the current level
-                current = current->next;
+                // Push the children of the current node into the queue
+                if (current->left != nullptr) q.push(current->left);
+                if (current->right != nullptr) q.push(current->right);
             }
-            
-            // Move to the next level (leftmost node of the next level)
-            level_start = level_start->left;
         }
         
-        return root;
+        return root;  // Return the modified tree
     }
 };
