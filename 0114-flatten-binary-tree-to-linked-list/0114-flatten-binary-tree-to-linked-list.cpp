f2eb3575c41b38227855967426e5831f3e0a35f7
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-10-04 22:27:14 +0530

    Time: 7 ms (34.40%), Space: 16.9 MB (46.16%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -12,22 +12,26 @@
 class Solution {
 public:
     void flatten(TreeNode* root) {
-        flatNode(root, nullptr);
-    }
+        TreeNode* curr = root;
 
-    TreeNode *flatNode(TreeNode* cur, TreeNode *pre) {
-        if(cur == nullptr) {
-            return pre;
+        while (curr != nullptr) {
+            if (curr->left != nullptr) {
+                // Find the rightmost node in the left subtree
+                TreeNode* pre = curr->left;
+                while (pre->right != nullptr) {
+                    pre = pre->right;
+                }
+                
+                // Connect the right subtree to the rightmost node of the left subtree
+                pre->right = curr->right;
+                
+                // Move the left subtree to the right
+                curr->right = curr->left;
+                curr->left = nullptr;
+            }
+            
+            // Move to the right subtree (which now includes the flattened left subtree)
+            curr = curr->right;
         }
-
-        TreeNode *right = cur->right;
-        if(pre != nullptr) {
-            pre->left = nullptr;
-            pre->right = cur;
-        }
-        
-        cur = flatNode(cur->left, cur);
-        cur = flatNode(right, cur);
-        return cur;
     }
 };
