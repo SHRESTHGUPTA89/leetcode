d420ebf7833659462e6d02f994c4effeb4c411ae
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-10-04 21:00:39 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (78.92%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -9,26 +9,41 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
 class Solution {
 public:
-    
-    
-    vector<int>ans;
-void  preOrder(TreeNode* root){
-if(root==NULL){
-    return;
-}
-
-ans.push_back(root->val);
-preOrder(root->left);    
-preOrder(root->right);
-
-
-}
     vector<int> preorderTraversal(TreeNode* root) {
-         preOrder(root);
-        return ans;
+        vector<int> result;
+        TreeNode* curr = root;
+        TreeNode* pre;
+
+        while (curr != nullptr) {
+            if (curr->left == nullptr) {
+                // Process the current node and move to the right
+                result.push_back(curr->val);
+                curr = curr->right;
+            } else {
+                // Find the inorder predecessor of the current node
+                pre = curr->left;
+                while (pre->right != nullptr && pre->right != curr) {
+                    pre = pre->right;
+                }
+                
+                // Set up the thread (temporary right link)
+                if (pre->right == nullptr) {
+                    pre->right = curr;
+                    result.push_back(curr->val); // Process the current node before moving left
+                    curr = curr->left;
+                } else {
+                    // Remove the thread (restore the original tree structure)
+                    pre->right = nullptr;
+                    curr = curr->right;
+                }
+            }
+        }
+
+        return result;
     }
 };
-
 
