1e6e7ed2e5eb0ae756dc7f2a0b423fc6039f1a46
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-10-05 14:24:15 +0530

    Time: 1075 ms (5.55%), Space: 60.7 MB (11.08%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
+        
+        sort(s1.begin(), s1.end());
+
         int len1 = s1.size();
         int len2 = s2.size();
 
+        
+        for (int i = 0; i <= len2 - len1; i++) {
+            
+            string r = s2.substr(i, len1);
+            
+            
+            sort(r.begin(), r.end());
 
-        if (len1 > len2) return false;
 
-        // Frequency arrays for s1 and for the sliding window in s2
-        vector<int> freqS1(26, 0), freqWindow(26, 0);
-
-        // Count frequencies of characters in s1
-        for (int i = 0; i < len1; i++) {
-            freqS1[s1[i] - 'a']++;
-            freqWindow[s2[i] - 'a']++;
-        }
-
-        // Check if the initial window matches
-        if (freqS1 == freqWindow) return true;
-
-        // Start sliding the window
-        for (int i = len1; i < len2; i++) {
-            // Add new character to the window
-            freqWindow[s2[i] - 'a']++;
-            // Remove the character that's sliding out of the window
-            freqWindow[s2[i - len1] - 'a']--;
-
-            // Check if the current window matches
-            if (freqS1 == freqWindow) return true;
+            if (r == s1) {
+                return true;
+            }
         }
 
         return false;
