ef939b073345405e0af16b6e6529982648591052
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-10-12 23:29:22 +0530

    Time: 267 ms (77.70%), Space: 92.7 MB (93.47%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
@@ -1,39 +1,26 @@
+//T.C : O(nlogn)
+//S.C : O(n)
 class Solution {
 public:
     int minGroups(vector<vector<int>>& intervals) {
-        vector<int> startTimes, endTimes;
-        
-    
-        for (auto &interval : intervals) {
-            startTimes.push_back(interval[0]);
-            endTimes.push_back(interval[1]);
-        }
-        
-    
-        sort(startTimes.begin(), startTimes.end());
-        sort(endTimes.begin(), endTimes.end());
-        
-        int n = intervals.size();
-        int activeGroups = 0, maxGroups = 0;
-        int i = 0, j = 0;
-        
-        
-        while (i < n) {
-            if (startTimes[i] <= endTimes[j]) {
-                
-                activeGroups++;
-                maxGroups = max(maxGroups, activeGroups);
-                i++;
-            } else {
-                
-                activeGroups--;
-                j++;
+        sort(begin(intervals), end(intervals));
+
+        priority_queue<int, vector<int>, greater<int>> pq; //stores end point of groups
+
+        for(vector<int>& interval : intervals) {
+            int start = interval[0];
+            int end   = interval[1];
+
+            if(!pq.empty() && pq.top() < start) {
+                pq.pop();
             }
+            pq.push(end);
         }
-        
-        return maxGroups;
+
+        return pq.size();
     }
 };
+
 
 
         
