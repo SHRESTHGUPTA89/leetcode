8c89b7ceb6be5569ef7477ae6046fb457b4e2bb6
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-11-05 22:56:59 +0530

    Time: 1 ms (54.26%), Space: 12.2 MB (16.88%) - LeetHub

--- 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -1,41 +1,12 @@
-
 class Solution {
 public:
     int minChanges(string s) {
-        char curr = s[0];
+        int ans = 0, i = 0;
 
-        int count = 0;
-        int changes = 0;
-        int n = s.length();
-
-        // Iterate through each character in the string
-        for (int i = 0; i < n; i++) {
-
-            if (s[i] == curr) {
-                count++;
-                continue;
-            }
-
-            if (count % 2 == 0) { //Even
-                count = 1;
-            } else { //Odd
-                count = 0;
-                changes++;
-            }
-            
-            curr = s[i];
+        while (i < s.length()) {
+            ans += (s[i++] ^ s[i++]) ? 1 : 0; // Adds 1 if characters are different
         }
 
-        return changes;
+        return ans;
     }
 };
-
-
-
-
-
-
-
-
-
-
