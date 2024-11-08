812f09c1f9cb6b08f3245905d013ef1970f3fc07
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-11-08 15:13:34 +0530

    Time: 0 ms (100.00%), Space: 95.5 MB (53.70%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
@@ -1,24 +1,30 @@
+//T.C : O(n)
+//S.C : O(1)
 class Solution {
 public:
     vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
         int n = nums.size();
 
-        vector<int> prefixXOR(n);
-        prefixXOR[0] = nums[0];
+        vector<int> result(n);
 
-        for (int i = 1; i < n; i++) {
-            prefixXOR[i] = prefixXOR[i - 1] ^ nums[i];
+        //step-1 : Find the total xor
+        int XOR = 0;
+        for(int i = 0; i < n; i++) {
+            XOR ^= nums[i];
         }
 
-        vector<int> ans(n);
-        int mask = (1 << maximumBit) - 1;
+        //To find flip, first find the mask having all bits set to 1
+        int mask = ((1 << maximumBit) - 1);
 
-        for (int i = 0; i < n; i++) {
-            // find k to maximize value
-            int currentXOR = prefixXOR[n - 1 - i];
-            ans[i] = currentXOR ^ mask;
+        for(int i = 0; i < n; i++) {
+            int k = XOR ^ mask; //this will give me the flipped value of XOR i.e. my best K
+            result[i] = k;
+
+            XOR = (XOR ^ nums[n-1-i]);
         }
 
-        return ans;
+        return result;
+
+
     }
 };
