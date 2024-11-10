99dc469a7e46913f4a5f68c0bdfc4d5c65ff7bfb
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-11-10 21:33:11 +0530

    Time: 4 ms (24.47%), Space: 14.9 MB (49.13%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -9,52 +9,32 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
 public:
+    map<int, map<int, multiset<int>>> m;
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        // Map to store nodes based on level distance and vertical
-        // map<level, map<vertical, multiset<node values>>>
-        map<int, map<int, multiset<int>>> nodes;
-        
-        // Queue to perform BFS
-        // Each element in the queue stores {node, level distance, vertical}
-        queue<pair<TreeNode*, pair<int, int>>> todo;
-        todo.push({root, {0, 0}}); // Start with root at level 0, vertical 0
-        
-        // Perform BFS
-        while (!todo.empty()) {
-            auto p = todo.front();
-            todo.pop();
-            TreeNode* node = p.first;
-            int level = p.second.first;
-            int vertical = p.second.second;
-            
-            // Store the node's value in the map
-            nodes[level][vertical].insert(node->val);
-            
-            // If the node has a left child, go to level-1 and vertical+1
-            if (node->left) {
-                todo.push({node->left, {level - 1, vertical + 1}});
+        queue<tuple<TreeNode*, int, int>> q;
+        q.push({root, 0, 0});
+
+        while (!q.empty()) { 
+            auto [temp, x, y] = q.front();
+            q.pop();
+            m[x][y].insert(temp->val);
+            if (temp->left) {
+                q.push({temp->left, x - 1, y + 1});
             }
-            
-            // If the node has a right child, go to level+1 and vertical+1
-            if (node->right) {
-                todo.push({node->right, {level + 1, vertical + 1}});
+            if (temp->right) {
+                q.push({temp->right, x + 1, y + 1});
             }
         }
-        
-        // Prepare the result
-        vector<vector<int>> result;
-        for (auto& p : nodes) {
+        vector<vector<int>> ans;
+        for (auto i : m) {
             vector<int> col;
-            for (auto& q : p.second) {
-                // Insert all the nodes in the current level and vertical
-                col.insert(col.end(), q.second.begin(), q.second.end());
+            for (auto j : i.second) {
+                col.insert(col.end(), j.second.begin(), j.second.end());
             }
-            result.push_back(col);
+            ans.push_back(col);
         }
-        
-        return result;
+        return ans;
     }
 };
