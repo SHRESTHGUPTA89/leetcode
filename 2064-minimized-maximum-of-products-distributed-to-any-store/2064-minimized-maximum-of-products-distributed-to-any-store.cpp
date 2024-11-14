4d4ecb1d7c2388159b7b7b0deecb26e80dc4388a
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-11-14 14:39:52 +0530

    Time: 23 ms (91.39%), Space: 87.3 MB (34.13%) - LeetHub

--- 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,35 +1,34 @@
-#include <vector>
-#include <cmath>
-#include <algorithm>
-
 class Solution {
-public:
-
-    bool happens(const std::vector<int>& quantities, int n, int mid) {
-        int count = 0;
-
-        for (int i = 0; i < quantities.size(); i++) {
-            // Correct way to calculate ceil division for integers
-            count += (quantities[i] + mid - 1) / mid;
+    private:
+        bool solve(int n, vector<int>& quantities, int item)
+        {
+            if(item == 0) return false;
+            int store = 0;
+            for(auto product : quantities)
+            {
+                store += (product - 1) / item + 1;
+                if(store > n) return false;
+            }
+            return true;
         }
-
-        return count <= n;
-    }
-
-    int minimizedMaximum(int n, std::vector<int>& quantities) {
-        int s = 1;
-        int e = *std::max_element(quantities.begin(), quantities.end());
-
-        while (s <= e) {
-            int mid = s + (e - s) / 2;
-
-            if (happens(quantities, n, mid)) {
-                e = mid - 1;
-            } else {
-                s = mid + 1;
+public:
+    int minimizedMaximum(int n, vector<int>& quantities) {
+        int low = 1;
+        int high = *max_element(quantities.begin(), quantities.end());
+        int ans = -1;
+        while(low <= high)
+        {
+            int mid = (low + high) / 2;
+            if(solve(n, quantities, mid))
+            {
+                ans = mid;
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
             }
         }
-
-        return s;
+        return ans;
     }
 };
