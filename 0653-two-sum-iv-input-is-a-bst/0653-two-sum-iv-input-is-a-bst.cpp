b36fd7249669bcabe16276a5466b8e86c8a5f89c
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-11-15 00:56:52 +0530

    Time: 7 ms (31.50%), Space: 38.8 MB (20.42%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -12,59 +12,25 @@
 class Solution {
 public:
     
-    void inorder(TreeNode* root ,   vector<int> &v){
-        
-        if(!root){
-            return;
+    bool seen(TreeNode* root, int k, unordered_set<int>& s) {
+        if (!root) {
+            return false;
         }
         
-        inorder(root->left,v);
-        v.push_back(root->val);
-        inorder(root->right,v);
+        
+        if (s.count(k - root->val)) {
+            return true;
+        }
+        
+    
+        s.insert(root->val);
         
         
+        return seen(root->left, k, s) || seen(root->right, k, s);
     }
+    
     bool findTarget(TreeNode* root, int k) {
-        
-        
-        // method 1 here we use inorder to make it sorted array than use simple to pointer 
-        
-        vector<int>v;
-        // v will be in sorted order , because inorder of BST is sorted 
-        inorder(root, v);
-        
-        
-        int l=0;
-        int r=v.size()-1;
-        
-        while(l<r){
-            int sum=v[l]+v[r];
-            if(sum==k){
-                return true;
-            }
-            else if(sum<k){
-                l++;
-            }
-            else{
-                r--;
-            }
-            
-        }
-        
-        return false;
+        unordered_set<int> s;
+        return seen(root, k, s);
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-   
