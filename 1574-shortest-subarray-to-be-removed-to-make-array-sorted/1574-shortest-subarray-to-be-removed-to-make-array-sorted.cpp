44f825e5b0252c7a01ea5219ccde8f43ec7f4a19
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-11-16 02:44:33 +0530

    Time: 0 ms (100.00%), Space: 69.3 MB (98.15%) - LeetHub

--- 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
     int findLengthOfShortestSubarray(vector<int>& arr) {
-        int right = arr.size() - 1;
-        while (right > 0 && arr[right] >= arr[right - 1]) {
-            right--;
+        int n = arr.size();
+
+        //step-1 - Find the jth pointer from right side
+        int j = n-1;
+        while(j > 0 && arr[j] >= arr[j-1]) {
+            j--;
         }
 
-        int ans = right;
-        int left = 0;
-        while (left < right && (left == 0 || arr[left - 1] <= arr[left])) {
-            // find next valid number after arr[left]
-            while (right < arr.size() && arr[left] > arr[right]) {
-                right++;
+        int i = 0;
+        int result = j; //we are removing all elements left to index j  becasue index j to n-1 are sorted
+        
+        //step-2 start finding correct i and j and find deleted elements = j - i - 1
+        while(i < j && (i == 0 || arr[i] >= arr[i-1])) { //increasing ordere of elements from i
+
+            //arr[j] < arr[i]
+            while(j < n && arr[i] > arr[j]) {
+                j++;
             }
-            // save length of removed subarray
-            ans = min(ans, right - left - 1);
-            left++;
+
+            //we have found correct jth element such that arr[i] <= arr[j]
+            result = min(result, j - i - 1);
+            i++;
         }
-        return ans;
+
+        return result;
+
     }
 };
