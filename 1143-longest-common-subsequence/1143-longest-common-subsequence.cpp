a5b22dc4e71199a2be8d50cd887ed89bcd74765e
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-11-23 19:28:14 +0530

    Time: 27 ms (60.89%), Space: 26.1 MB (30.18%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,27 +1,27 @@
-//T.C : O(m*n)
-//S.C : O(m*n)
-//Approach-1 (Recursion + Memoization)
+// T.C: O(m*n)
+// S.C: O(m*n)
+// Approach-2 (Bottom-Up Dynamic Programming)
 class Solution {
 public:
-    int t[1001][1001];
-    int LCS(string& s1, string& s2, int m, int n) {
-        if(m == 0 || n == 0)
-            return t[m][n] = 0;
-        
-        if(t[m][n] != -1)
-            return t[m][n];
-        
-        if(s1[m-1] == s2[n-1])
-            return t[m][n] = 1 + LCS(s1, s2, m-1, n-1);
-        
-        return t[m][n] = max(LCS(s1, s2, m, n-1), LCS(s1, s2, m-1, n));
-    }
     int longestCommonSubsequence(string text1, string text2) {
         int m = text1.length();
         int n = text2.length();
         
-        memset(t, -1, sizeof(t));
+        // Create a DP table
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
         
-        return LCS(text1, text2, m, n);
+        // Build the DP table
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (text1[i - 1] == text2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        
+        // Return the value in the bottom-right cell of the table
+        return dp[m][n];
     }
 };
