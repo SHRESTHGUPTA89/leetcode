9f7c333babd5d2bfb94da0a36d30ed9c1635be62
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-11-29 21:36:07 +0530

    Time: 0 ms (100.00%), Space: 17.1 MB (28.73%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,46 +1,17 @@
 class Solution {
 public:
-    long getAns(long *Arr, int ind, int buy, int n, vector<vector<long>> &dp) {
-        // Base case: No transactions possible after the last day
-        if (ind == n) {
-            return 0;
-        }
-
-        // Return cached result if already computed
-        if (dp[ind][buy] != -1) {
-            return dp[ind][buy];
-        }
-
-        long profit = 0;
-
-        if (buy == 0) { // Can buy the stock
-            profit = max(
-                0 + getAns(Arr, ind + 1, 0, n, dp),       // Skip buying
-                -Arr[ind] + getAns(Arr, ind + 1, 1, n, dp) // Buy the stock
-            );
-        } else { // Can sell the stock
-            profit = max(
-                0 + getAns(Arr, ind + 1, 1, n, dp),       // Skip selling
-                Arr[ind] + getAns(Arr, ind + 1, 0, n, dp)  // Sell the stock
-            );
-        }
-
-        // Cache and return the computed profit
-        return dp[ind][buy] = profit;
-    }
-
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        if (n == 0) return 0; // Edge case: No prices
-
-        // Convert prices to a long array
-        vector<long> pricesLong(prices.begin(), prices.end());
-
-        // DP table for memoization
-        vector<vector<long>> dp(n, vector<long>(2, -1));
-
-        // Compute maximum profit starting from day 0 with "can buy" state
-        long ans = getAns(&pricesLong[0], 0, 0, n, dp);
-        return ans;
+        
+        int profit=0;
+        int n=prices.size();
+        for(int i=1;i<n;i++){
+            
+            if(prices[i]>prices[i-1]){
+                profit+=prices[i]-prices[i-1];
+            }
+        }
+        
+        return profit;
+        
     }
 };
