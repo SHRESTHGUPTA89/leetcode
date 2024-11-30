76e541180a70a42f077280fe66f94840120aaa0c
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-11-30 21:51:59 +0530

    Time: 0 ms (100.00%), Space: 52.3 MB (14.09%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         
+   //      greedy method
+        
+        // hum loog end ko laga taar last se first move kar rahe for detail watch nikhal lohia
         int n=nums.size();
         int start=0;
         int end=n-1;
