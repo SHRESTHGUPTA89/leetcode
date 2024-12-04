261022a914e10aa4fc51e48fbac42a12e071b2be
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-12-05 01:28:16 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (18.34%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,38 +1,39 @@
+//Approach-3 (Using two Stacks) - No DP required
+//T.C : O(n)
+//S.C : O(n)
 class Solution {
-public:
-    
-    int t[101][101];
-    bool solve(int idx, int open, string& s, int n) {
-        if(idx == n) {
-            return open == 0;
-        }
-        
-        if(t[idx][open] != -1) {
-            return t[idx][open] == 1 ? true : false;
-        }
-        
-        bool isValid = false;
-        
-        if(s[idx] == '*') {
-            if(open > 0)
-                isValid |= solve(idx+1, open-1, s, n); //Treating * as )
-            
-            isValid |= solve(idx+1, open+1, s, n); //Treating * as (
-            
-            isValid |= solve(idx+1, open, s, n);   //Treating * as ''
-        } else if(s[idx] == '(') {
-            isValid |= solve(idx+1, open+1, s, n);
-        } else if(open > 0) {
-            isValid |= solve(idx+1, open-1, s, n);
-        }
-        
-        return t[idx][open] = isValid;
-    }
-    
+public: 
     bool checkValidString(string s) {
-        int n = s.length();
-        memset(t, -1, sizeof(t));
-        return solve(0, 0, s, n);
-        
+        stack<int> openSt;
+        stack<int> asterisksSt;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s[i];
+
+            if (ch == '(') {
+                openSt.push(i);
+            } else if (ch == '*') {
+                asterisksSt.push(i);
+            } else {
+                if (!openSt.empty()) {
+                    openSt.pop();
+                } else if (!asterisksSt.empty()) {
+                    asterisksSt.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        //This post processing will be required for cases like - "*(())(*"
+        while (!openSt.empty() && !asterisksSt.empty()) {
+            if (openSt.top() > asterisksSt.top()) {
+                return false;
+            }
+            openSt.pop();
+            asterisksSt.pop();
+        }
+
+        return openSt.empty();
     }
 };
