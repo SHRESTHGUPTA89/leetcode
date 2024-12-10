ccb3df0a83961ac0b71c183571506e837114af20
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-12-10 14:00:55 +0530

    Time: 6 ms (75.93%), Space: 126.6 MB (66.25%) - LeetHub

--- 3152-special-array-ii/3152-special-array-ii.cpp
+++ 3152-special-array-ii/3152-special-array-ii.cpp
@@ -1,26 +1,36 @@
+//Approach-1 (Using cumulative sum)
+//T.C : O(n + m)
+//S.C : O(n)
 class Solution {
 public:
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
         int n = nums.size();
-        vector<int> prefix(n, 0); // Initialize prefix array with size n
-        
-        // Calculate the prefix array
-        for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i - 1];
-            if ((nums[i - 1] % 2 == 0 && nums[i] % 2 == 0) || (nums[i - 1] % 2 != 0 && nums[i] % 2 != 0)) {
-                prefix[i]++;
+        int m = queries.size();
+
+        vector<int> cumSum(n, 0);
+        //cumSum[i] = total count of violating indices till index i
+        cumSum[0] = 0;
+
+        for(int i = 1; i < n; i++) {
+            if(nums[i]%2 == nums[i-1]%2) { //violating index 
+                cumSum[i] = cumSum[i-1]+1;
+            } else {
+                cumSum[i] = cumSum[i-1];
             }
         }
 
-        vector<bool> ans; // Result vector
-        
-        // Process the queries
-        for (auto& q : queries) {
-            int left = q[0], right = q[1];
-            int specialCount = prefix[right] - (left > 0 ? prefix[left] : 0);
-            ans.push_back(specialCount == 0);
+        vector<bool> result(m, false);
+        int i = 0;
+        for(vector<int>& query : queries) {
+            int start = query[0];
+            int end = query[1];
+
+            if(cumSum[end] - cumSum[start] == 0) {
+                result[i] = true;
+            }
+            i++;
         }
 
-        return ans; // Return the result
+        return result;
     }
 };
