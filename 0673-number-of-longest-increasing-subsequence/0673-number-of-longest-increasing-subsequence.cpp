3784415eb15fec2e77c8f86320f6ae4bfdab23f1
SHRESTHGUPTA89 <104047032+SHRESTHGUPTA89@users.noreply.github.com>
2024-12-26 20:26:06 +0530

    Time: 31 ms (57.85%), Space: 17.6 MB (18.98%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -1,37 +1,41 @@
+
+//Using same code of LIS - (T.C : O(n^2))
 class Solution {
 public:
-    int findNumberOfLIS(vector<int>& arr) {
+    int findNumberOfLIS(vector<int>& nums) {
+        int n = nums.size();
         
-    int n = arr.size();
-
-    vector<int> dp(n, 1); // dp[i] stores the length of the LIS ending at arr[i]
-    vector<int> ct(n, 1); // ct[i] stores the count of LIS ending at arr[i]
-
-    int maxi = 1; // Initialize the maximum length as 1
-
-    for (int i = 0; i < n; i++) {
-        for (int prev_index = 0; prev_index < i; prev_index++) {
-            if (arr[prev_index] < arr[i] && dp[prev_index] + 1 > dp[i]) {
-                dp[i] = dp[prev_index] + 1;
-                // Inherit count
-                ct[i] = ct[prev_index];
-            } else if (arr[prev_index] < arr[i] && dp[prev_index] + 1 == dp[i]) {
-                // Increase the count
-                ct[i] = ct[i] + ct[prev_index];
+        vector<int> t(n, 1);
+        vector<int> count(n, 1);
+        
+        for(int i = 0; i<n; i++) {
+            
+            for(int j = 0; j<i; j++) {
+                
+                if(nums[j] < nums[i]) {
+                    
+                    if(t[i] == t[j] + 1)
+                        count[i] += count[j];
+                    else if(t[i] < t[j] + 1){
+                        t[i] = t[j] + 1;
+                        count[i] = count[j];
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+        int maxVal = *max_element(begin(t), end(t));
+        
+        int result = 0;
+        for(int i = 0; i<n; i++) {
+            if(t[i] == maxVal) {
+                result += count[i];
             }
         }
-        maxi = max(maxi, dp[i]);
+        
+        return result;
     }
-
-    int numberOfLIS = 0;
-
-    for (int i = 0; i < n; i++) {
-        if (dp[i] == maxi) {
-            numberOfLIS += ct[i];
-        }
-    }
-
-    return numberOfLIS;
-}
-    
 };
